d87df2fb66e1421066f1744869fecaf0431e4e1c
Protokollmaker <re08win@gmail.com>
2023-03-11 15:38:06 +0100

    more stabel version need more imp

--- backend/Readme.md
+++ backend/Readme.md
@@ -1,2 +1,4 @@
-For Lookin up exception codes
+### For Lookin up exception codes
 - https://arduino-esp8266.readthedocs.io/en/latest/exception_causes.html
+### Inspreation
+- https://randomnerdtutorials.com/esp8266-nodemcu-websocket-server-arduino/
--- backend/main/main.ino
+++ backend/main/main.ino
@@ -1,169 +1,181 @@
-#include "config.h"
-#ifdef ESP32
-  #include <WiFi.h>
-  #include <AsyncTCP.h>
-#else
-  #include <ESP8266WiFi.h>
-  #include <ESPAsyncTCP.h>
-#endif
+// Import required libraries
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include "password.h"
+#include "Ticker.h"
 #include <ArduinoJson.h>
 
 // Replace with your network credentials
 const char* ssid = SSID_WLAN;
 const char* password = PASSWORD_WLAN;
 
+#define TIMER_NUMBER 5
+int timer[TIMER_NUMBER];
+
+bool ledState = 0;
+const int ledPin = 2;
+
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
-AsyncWebSocket ws("/ws"); 
+AsyncWebSocket ws("/ws");
 
-struct Timer {
-    int timerLeft = 0;
-    int relay = 0;
-    bool state = 0;
-    bool run = 0;
-};
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <title>ESP Web Server</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<title>ESP Web Server</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="data:,">
+</head>
+<body>
+  <div class="topnav">
+    <h1>ESP WebSocket Server</h1>
+  </div>
+  <div class="content">
+    <div class="card">
+      <h2>Output - GPIO 2</h2>
+      <p class="state">state: <span id="state">Timer</span></p>
+      <p class="state">state: <span id="state">%STATE%</span></p>
+      <p><button id="button" class="button">Toggle</button></p>
+    </div>
+  </div>
+</body>
+</html>
+)rawliteral";
 
-Timer timerlist[NUMBER_OF_TIMERS];
-
-bool timerIsSet(int timerID){
-    if (timerID < 0 || timerID > NUMBER_OF_TIMERS) {Serial.println("[Timer] Out of index"); return 0;}
-    if(timerlist[timerID].timerLeft);
-        return 1;
-    return 0;
+void printMessage(){
+    Serial.printf("test");
 }
 
-Timer getTimer(int timerID, bool &error){
-    if (timerID < 0 || timerID > NUMBER_OF_TIMERS) {
-        Serial.println("[Timer] Out of index");
-        error = 1;
-        return timerlist[0];
+//Ticker timer1(printMessage, 500);
+//int tiemr = 0;
+
+void notifyClients() {
+  ws.textAll(String(ledState));
+}
+
+
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    data[len] = 0;
+    ws.textAll((char*)data);
+    StaticJsonDocument<128> doc;
+    DeserializationError err = deserializeJson(doc, (char*)data, len);
+    if (err) {
+      Serial.print(F("[JSON] deserializeJson() failed: "));
+      Serial.println(err.c_str());
+      return;
     }
-    return timerlist[timerID];
+    const char* eventtype = doc["eventtype"];
+    if   (!strcmp(eventtype, "setTimer")) {
+      if (doc["timerID"].as<int>() < TIMER_NUMBER && doc["timerID"].as<int>() >= 0) //check in bounse
+      {
+        timer[doc["timerID"].as<int>()] = doc["time"].as<int>();
+      }
+    }
+    if (!strcmp(eventtype, "setRelay")){
+
+    }
+    /*if (strcmp((char*)data, "toggle") == 0) {
+      ledState = !ledState;
+      digitalWrite(ledPin, ledState);
+      notifyClients();
+    }*/
+  }
 }
 
-bool setTimer(Timer timer, int timerID){
-    if (timerID < 0 || timerID > NUMBER_OF_TIMERS) {Serial.println("[Timer] Out of index"); return 1;}
-    timerlist[timerID] = timer;
-    return 0;
-}
-
-bool setNewTimer(Timer timer, int &timerIndex){
-    timerIndex = -1;
-    for (int i = 0; i < NUMBER_OF_TIMERS; i++){
-        if (!timerIsSet(i)){continue;}
-        timerIndex = i;
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
+             void *arg, uint8_t *data, size_t len) {
+    switch (type) {
+      case WS_EVT_CONNECT:
+        Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
         break;
-    }
-    if (timerIndex == -1){
-        Serial.print("[Timer] no Timer Free");
-        return 1;
-    }
-    return setTimer(timer, timerIndex);
+      case WS_EVT_DISCONNECT:
+        Serial.printf("WebSocket client #%u disconnected\n", client->id());
+        break;
+      case WS_EVT_DATA:
+        handleWebSocketMessage(arg, data, len);
+        break;
+      case WS_EVT_PONG:
+      case WS_EVT_ERROR:
+        break;
+  }
 }
 
-bool delTimer(int timerID) {
-    if (timerID < 0 || timerID > NUMBER_OF_TIMERS) {Serial.println("[Timer] Out of index"); return 1;}
-    timerlist[timerID].timerLeft = 0;
-    return 0;
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
 }
 
-void onEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
-    if(type == WS_EVT_CONNECT){return;}
-    if(type == WS_EVT_DISCONNECT){ return;}
-    if(type == WS_EVT_ERROR){ return;}
-    if(type == WS_EVT_PONG){return;}
-    if(type == WS_EVT_DATA){
-        for(size_t i=0; i<len; i++){
-            Serial.write(data[i]);
-        }
-        Serial.println();
-        StaticJsonDocument<128> doc;
-        DeserializationError err = deserializeJson(doc, (char*)data, len);
-        if (err) {
-            Serial.print(F("[JSON] deserializeJson() failed: "));
-            Serial.println(err.c_str());
-            return;
-        }
-        const char* eventtype = doc["eventtype"];
-        if   (!strcmp(eventtype, "setTimer")) {
-            Timer timer = {doc["time"], doc["Relay"], doc["turn"], doc["run"]};
-            int newTimerIndex;
-            if (setNewTimer(timer, newTimerIndex))
-                return; // Error 
-            return;
-        }
-        if   (!strcmp(eventtype, "getTimers")) {
-            for (int i = 0; i < NUMBER_OF_TIMERS; i++){
-                // ! implement
-            }
-        }
-        doc.clear();
-        return;
+String processor(const String& var){
+  Serial.println(var);
+  if(var == "STATE"){
+    if (ledState){
+      return "ON";
     }
+    else{
+      return "OFF";
+    }
+  }
+  return String();
 }
+
+/*String processor1(const String& var){
+  return "02139"
+}*/
 
 void setup(){
-    // Serial port for debugging purposes
-    Serial.begin(115200);
-    // Connect to Wi-Fi
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(SSID_WLAN, PASSWORD_WLAN);
-    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-        Serial.printf("[Wifi] WiFi Failed!\n");
-        return;
-    }
-    Serial.println("[Wifi] IP address: ");
-    Serial.println(WiFi.localIP());
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  for (int i = 0; i < TIMER_NUMBER; i++){
+    timer[i] = 0;
+  }
 
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
 
-    // Route for root / web page
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-        request->send(200, "text/plain", "Test");
-    });
-    //server.onNotFound(onRequest);
-    DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
-    ws.onEvent(onEvent);
-    server.addHandler(&ws);
-    server.onNotFound([](AsyncWebServerRequest *request) {
-        if (request->method() == HTTP_OPTIONS) {
-            request->send(200);
-        } else {
-            request->send(404);
-        }
-    });
-    server.begin();
+  // Print ESP Local IP Address
+  Serial.println(WiFi.localIP());
+
+  initWebSocket();
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+
+  // Start server
+  server.begin();
 }
-bool timerState = 1;
 
 void loop() {
-    if (timerState) { timerState = 0; } 
-    else            { timerState = 1; }
-    
-    for (int i = 0; i < NUMBER_OF_TIMERS; i++){
-        if (timerlist[i].timerLeft <= 0)
-            continue;
-        if (!timerlist[i].run)
-            continue;
-        if (!timerlist[i].timerLeft){
-            delTimer(i);
-            //digitalWrite(blink_pins[timerlist[i].relay],LOW);
-        }
-        timerlist[i].timerLeft = timerlist[i].timerLeft - 1;
-        //digitalWrite(blink_pins[timerlist[i].relay],timerState);
-
-        Serial.print("[TIMER] Timer : ");
-        Serial.print(i);
-        Serial.print(" : TimerLeft : ");
-        Serial.println(timerlist[i].timerLeft);
+  ws.cleanupClients();
+  for (int i = 0; i < TIMER_NUMBER; i++){
+    if (timer[i] == -1){
+      continue;
+    }
+    if (timer[i] == 0)  {
+      ws.textAll((char*)"data");
     }
     
-    StaticJsonDocument<32> event;
-    event["eventtype"] = "Tick";
-    String output;
-    serializeJson(event, output);
-    ws.textAll(output);
+    Serial.print("Timer: ");
+    Serial.print(i);
+    Serial.print(": Timer: ");
+    Serial.println(timer[i]);
 
-    ws.cleanupClients();
-    delay(1000);
+    timer[i] = timer[i] -1;
+  }
+  ws.textAll("Tick");
+  delay(1000);
 }
